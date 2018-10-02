a0b9d75a17d47282d7b7ee26f2d96b75b0a06fcc
Anthony Curtis <tonydc1997@gmail.com>
2018-10-02 07:17:19 -0700

    Create function that POSTS something to server

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const userInputHTML = (input) => {
   let textnode = document.createTextNode(input);
   p.appendChild(textnode);
 }
+
+const sentToServer = () => {
+  const input = document.querySelector('#userInput').value;
+  userInputHTML(input);
+  fetch('http://localhost:3000/secret', {
+    method: 'POST',
+    body: JSON.stringify({userInput: input}),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+}
